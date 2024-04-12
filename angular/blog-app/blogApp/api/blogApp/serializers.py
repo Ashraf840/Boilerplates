d5cf90dfb89fd299fd5feb15339767a04b89c801
Ashraf840 <shift2unleashed2011@gmail.com>
2024-04-12 20:03:10 +0600

    Added a new field to PostSerializer to return author profile_pic from the backend; Showing author profile_pic in the frontend

--- angular/blog-app/blogApp/api/blogApp/serializers.py
+++ angular/blog-app/blogApp/api/blogApp/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+import src.settings as settings
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,7 +8,26 @@ class PostSerializer(serializers.ModelSerializer):
     tag = serializers.StringRelatedField(many=True)
     category = serializers.StringRelatedField(many=True)
     author = serializers.StringRelatedField()
+    author_profile_picture = serializers.SerializerMethodField()
     
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = [
+            'id', 'tag', 'category', 'author', 'title', 'content', 
+            'readingTime', 'banner_image', 'status', 'created_at',
+            'updated_at', 'author_profile_picture',
+        ]
+
+    def get_author_profile_picture(self, obj):
+        # 'author' is the foreign key field in the 'Post' model which points to 'User' model, thus accessing user 'profile_pic' URL using 'traversing relations'.
+        profile_picture_url = obj.author.profile_pic.url
+
+        # Since the user profile_pic URL is not returning the absolute URL unlike banner_image of the blog post, I'm programatically getting the request schema & request host to append them with the profile_pic URL at the end.
+        request = self.context.get('request', None)
+
+        if request is not None and request.scheme and request.get_host():
+            complete_image_url = f"{request.scheme}://{request.get_host()}{profile_picture_url}"
+            profile_picture_url = complete_image_url
+            return profile_picture_url
+        else:
+            return profile_picture_url
