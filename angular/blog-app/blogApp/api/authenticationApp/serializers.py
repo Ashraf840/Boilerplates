39d32767ea12623348fa64dd494d3fb8aabf7d6f
Ashraf840 <shift2unleashed2011@gmail.com>
2024-04-14 03:57:47 +0600

    Get the aosulte URL of post banner image from backend api; load the images into angular client;

--- angular/blog-app/blogApp/api/authenticationApp/serializers.py
+++ angular/blog-app/blogApp/api/authenticationApp/serializers.py
@@ -7,10 +7,23 @@ from django.core import serializers as djc_serializer
 class PostSerializer(serializers.ModelSerializer):
 
     category = serializers.StringRelatedField(many=True)
+    banner_image_absolute_url = serializers.SerializerMethodField()
     
     class Meta:
         model = Post
-        fields = ['id', 'banner_image', 'category', 'title', 'created_at', 'content']
+        fields = ['id', 'banner_image_absolute_url', 'category', 'title', 'created_at', 'content']
+
+    def get_banner_image_absolute_url(self, obj):
+        banner_image_url = obj.banner_image
+
+        request = self.context.get('request', None)
+
+        if request is not None and request.scheme and request.get_host():
+            complete_image_url = f"{request.scheme}://{request.get_host()}/media/{banner_image_url}"
+            banner_image_url = complete_image_url
+            return banner_image_url
+        else:
+            return banner_image_url
 
 class AuthorSerializer(serializers.ModelSerializer):
 
@@ -22,7 +35,8 @@ class AuthorSerializer(serializers.ModelSerializer):
 
     def get_author_articles(self, obj):
         articles = Post.objects.filter(author__username=obj.username)
-        article_serializer = PostSerializer(articles, many=True)
+        request = self.context['request']
+        article_serializer = PostSerializer(articles, many=True, context={'request': request})
         return article_serializer.data
         articles_data = djc_serializer.serialize('json', articles)
         return articles_data
