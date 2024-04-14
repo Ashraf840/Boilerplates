3dd7a15802c8b0e05ffc0e8ec229890342be6185
Ashraf840 <shift2unleashed2011@gmail.com>
2024-04-14 20:35:56 +0600

    Created a new angular app for practice pagination

---
+++ angular/pagination/paginationApp/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PaginationComponent } from './pagination/pagination.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PaginationComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
